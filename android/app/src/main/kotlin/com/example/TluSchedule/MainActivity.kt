fbaffa44035ebff7c0178b021643f6308cf9cb69
TuCoi <tudd72@wru.vn>
2020-07-07 21:52:53 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/TluSchedule/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.TluSchedule
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:TluSchedule/screens/loginscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Main());
+}
+
+class Main extends StatefulWidget {
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TLU Schedule',
+      home: Login(),
+    );
+  }
+}
